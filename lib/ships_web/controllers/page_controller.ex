c85c4ed5284a04206d4a1d0a4a550a1a90ce8b3c
lucasx <l.kaminski@protonmail.com>
2023-02-27 11:01:12 +0100

    Add index page with player and enemy board

--- lib/ships_web/controllers/page_controller.ex
+++ lib/ships_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule ShipsWeb.PageController do
   use ShipsWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    board_range = 0..9
+    render(conn, "index.html",
+    row: board_range,
+    col: board_range
+  )
   end
 end
