b4b238af1a20d6ca6aec0513d3d310a64717508d
lucasx <l.kaminski@protonmail.com>
2023-03-01 11:06:52 +0100

    Add a function to Game module to shoot at the opposite player

--- lib/ships/core/game.ex
+++ lib/ships/core/game.ex
@@ -50,11 +50,146 @@ defmodule Ships.Core.Game do
     {response, game, ship_coordinates}
   end
 
+  @spec shoot(%__MODULE__{}, any(), {non_neg_integer(), non_neg_integer()}) ::
+          {atom(), %__MODULE__{}, list}
+  def shoot(%__MODULE__{} = game, player_id, coordinates) do
+    {shooter, opponent} = assign_roles(game, player_id)
+
+    {response, updated_shooter, updated_opponent, response_coordinates} =
+      with :allowed <- is_player_allowed(game, player_id),
+           false <- already_used?(shooter, coordinates),
+           {:hit, updated_shooter, updated_opponent} <-
+             take_a_shot(shooter, opponent, coordinates),
+           {ship_status, ship_coordinates} <- check_if_destroyed(updated_opponent, coordinates),
+           :not_yet <- all_ships_destroyed(updated_opponent) do
+        {ship_status, updated_shooter, updated_opponent, ship_coordinates}
+      else
+        :not_allowed -> {:not_your_turn, shooter, opponent, [coordinates]}
+        true -> {:used, shooter, opponent, [coordinates]}
+        {:miss, updated_shooter} -> {:miss, updated_shooter, opponent, [coordinates]}
+        :all_destroyed -> {:game_over, shooter, opponent, [coordinates]}
+      end
+
+    game =
+      update_game(game, updated_shooter, updated_opponent)
+      |> assign_next_turn(response)
+
+    {response, game, response_coordinates}
+  end
+
+  defp is_player_allowed(game, player_id) do
+    allowed_player = game.turn
+
+    shooter =
+      cond do
+        game.player1.id == player_id -> :player1
+        game.player2.id == player_id -> :player2
+      end
+
+    case shooter == allowed_player do
+      true -> :allowed
+      false -> :not_allowed
+    end
+  end
+
+  defp update_game(game, shooter, opponent) do
+    case shooter.id == game.player1.id do
+      true -> %{game | player1: shooter, player2: opponent}
+      false -> %{game | player2: shooter, player1: opponent}
+    end
+  end
+
+  defp all_ships_destroyed(opponent) do
+    case length(opponent.got_hit_at) == Enum.sum(@available_ships) do
+      true -> :all_destroyed
+      false -> :not_yet
+    end
+  end
+
+  defp check_if_destroyed(player, coordinates) do
+    player_ships = Map.get(player, :ships)
+    ship_coordinates = Enum.find(player_ships, fn ship -> Enum.member?(ship, coordinates) end)
+    hit_coordinates = Map.get(player, :got_hit_at)
+
+    result =
+      Enum.all?(ship_coordinates, fn coordinate -> Enum.member?(hit_coordinates, coordinate) end)
+
+    case result do
+      true -> {:destroyed, ship_coordinates}
+      _ -> {:hit, [coordinates]}
+    end
+  end
+
+  defp take_a_shot(shooter, opponent, coordinates) do
+    opponent_ships = Map.get(opponent, :ships)
+    updated_shooter = update_shot_coordinates(shooter, coordinates)
+    shot_result = check_if_hit(opponent_ships, coordinates)
+
+    case shot_result do
+      true ->
+        updated_opponent = update_hit_at(opponent, coordinates)
+        {:hit, updated_shooter, updated_opponent}
+
+      _ ->
+        {:miss, updated_shooter}
+    end
+  end
+
+  defp update_shot_coordinates(player, coordinates) do
+    Map.update!(player, :shot_at, fn shot_at -> [coordinates | shot_at] end)
+  end
+
+  defp update_hit_at(player, coordinates) do
+    Map.update!(player, :got_hit_at, fn hit_coordinates ->
+      [coordinates | hit_coordinates]
+    end)
+  end
+
+  defp check_if_hit(ships_coordinates, shot_coordinates) do
+    Enum.any?(ships_coordinates, fn coordinates ->
+      Enum.member?(coordinates, shot_coordinates)
+    end)
+  end
+
+  defp already_used?(shooter, coordinates) do
+    Enum.member?(shooter.shot_at, coordinates)
+  end
+
+  defp assign_next_turn(game, shot_response) do
+    turn =
+      case shot_response do
+        :hit -> game.turn
+        :miss -> change_turn(game.turn)
+        :used -> game.turn
+        :destroyed -> game.turn
+        :game_over -> game.turn
+        :not_your_turn -> game.turn
+      end
+
+    %{game | turn: turn}
+  end
+
+  defp change_turn(turn) do
+    case turn do
+      :player1 -> :player2
+      :player2 -> :player1
+    end
+  end
+
+  defp assign_roles(%__MODULE__{} = game, player_id) do
+    {shooter, opponent} =
+      cond do
+        player_id == game.player1.id -> {game.player1, game.player2}
+        player_id == game.player2.id -> {game.player2, game.player1}
+      end
+
+    {shooter, opponent}
+  end
+
   defp get_player(%__MODULE__{} = game, player_id) do
     cond do
       game.player1.id == player_id -> {:player1, game.player1}
       game.player2.id == player_id -> {:player2, game.player2}
-      true -> nil
     end
   end
 
--- test/ships/core/game_test.exs
+++ test/ships/core/game_test.exs
@@ -143,87 +143,283 @@ defmodule Ships.Core.GameTest do
   end
 
   describe "place_ship(game, player_id, coordinates, orientation) when player already placed all of his ships" do
-    test "should return {:all_placed, game, []} with unchanged player" do
-      player_id = "player1"
-      game = Game.new_game(player_id)
-      orientation = :horizontal
+    setup [:place_all_ships]
 
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {0, 0}, orientation)
+    test "should return {:all_placed, game, []} with unchanged player", context do
+      {response, reponse_game, _coordinates_after} =
+        Game.place_ship(context.game_all_placed, context.player1, {8, 8}, :horizontal)
 
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {0, 2}, orientation)
-
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {0, 4}, orientation)
-
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {0, 6}, orientation)
-
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {0, 8}, orientation)
-
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {6, 0}, orientation)
-
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {6, 2}, orientation)
-
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {6, 4}, orientation)
-
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {6, 6}, orientation)
-
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {6, 8}, orientation)
-
-      {response, game_after, _coordinates_after} =
-        Game.place_ship(game, player_id, {8, 8}, orientation)
-
-      assert game_after.player1 == game.player1
+      assert reponse_game.player1 == context.game_all_placed.player1
       assert response == :all_placed
     end
   end
 
   describe "place_ship(game, player_id, coordinates, orientation) when player places his last ship" do
-    test "should change player's status to :ready" do
-      player_id = "player1"
-      game = Game.new_game(player_id)
-      orientation = :horizontal
+    setup [:place_all_ships]
 
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {0, 0}, orientation)
-
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {0, 2}, orientation)
-
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {0, 4}, orientation)
-
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {0, 6}, orientation)
-
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {0, 8}, orientation)
-
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {6, 0}, orientation)
-
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {6, 2}, orientation)
-
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {6, 4}, orientation)
-
-      {_response, game, _coordinates_after} =
-        Game.place_ship(game, player_id, {6, 6}, orientation)
-
-      {_response, game_after, _coordinates_after} =
-        Game.place_ship(game, player_id, {6, 8}, orientation)
-
-      assert game.player1.status != game_after.player1.status
-      assert game_after.player1.status == :ready
+    test "should change player's status to :ready", context do
+      assert context.game.player1.status != context.game_all_placed.player1.status
+      assert context.game_all_placed.player1.status == :ready
     end
+  end
+
+  describe "shoot(game, player_id, coordinates) when ship was hit" do
+    setup [:place_one_ship]
+
+    test "should return {:hit, game, coordinates}", context do
+      {response, _game, _coordinates} =
+        Game.shoot(context.game, context.player1, context.coordinates)
+
+      assert response == :hit
+    end
+
+    test "should return list with shot at coordinates", context do
+      {_response, _game, coordinates} =
+        Game.shoot(context.game, context.player1, context.coordinates)
+
+      assert coordinates == [context.coordinates]
+    end
+
+    test "should update opponent's got_hit_at list", context do
+      {_response, game_after, _coordinates} =
+        Game.shoot(context.game, context.player1, context.coordinates)
+
+      opponent = game_after.player2
+
+      assert opponent.got_hit_at == [{0, 0}]
+    end
+
+    test "should update shooter's shot_at list", context do
+      {_response, game_after, _coordinates} =
+        Game.shoot(context.game, context.player1, context.coordinates)
+
+      shooter = game_after.player1
+
+      assert shooter.shot_at == [{0, 0}]
+    end
+
+    test "should not change game turn to another player", context do
+      {_response, game_after, _coordinates} =
+        Game.shoot(context.game, context.player1, context.coordinates)
+
+      assert game_after.turn == :player1
+    end
+  end
+
+  describe "shoot(game, player_id, coordinates) when coordinate was already shot at" do
+    setup [:place_one_ship]
+
+    test "should return {:used, game, coordinates}", context do
+      {_response, game, _coordinates} =
+        Game.shoot(context.game, context.player1, context.coordinates)
+
+      {response, _game, _coordinates} = Game.shoot(game, context.player1, context.coordinates)
+
+      assert response == :used
+    end
+
+    test "should not change game turn to another player", context do
+      {_response, game, _coordinates} =
+        Game.shoot(context.game, context.player1, context.coordinates)
+
+      {_response, game_after, _coordinates} =
+        Game.shoot(game, context.player1, context.coordinates)
+
+      assert game_after.turn == :player1
+    end
+  end
+
+  describe "shoot(game, player_id, coordinates) when shot missed the ship" do
+    setup [:place_one_ship]
+
+    test "should return {:miss, game, coordinates}", context do
+      {response, _game, _coordinates} = Game.shoot(context.game, context.player1, {8, 8})
+
+      assert response == :miss
+    end
+
+    test "should update shooter's shot_at list", context do
+      {_response, game_after, _coordinates} = Game.shoot(context.game, context.player1, {8, 8})
+      shooter = game_after.player1
+
+      assert shooter.shot_at == [{8, 8}]
+    end
+
+    test "should not update opponent's got_hit_at list", context do
+      {_response, game_after, _coordinates} = Game.shoot(context.game, context.player1, {8, 8})
+      opponent = game_after.player2
+
+      assert opponent.got_hit_at == []
+    end
+
+    test "should change game turn to another player", context do
+      {_response, game_player1, _coordinates} = Game.shoot(context.game, context.player1, {8, 8})
+      {_response, game_player2, _coordinates} = Game.shoot(game_player1, context.player2, {8, 8})
+
+      assert game_player1.turn == :player2
+      assert game_player2.turn == :player1
+    end
+  end
+
+  describe "shoot(game, player_id, coordinates) when ship was hit and destroyed" do
+    setup [:place_one_ship, :shoot_three_times]
+
+    test "should return {:destroyed, game, coordinates}", context do
+      {response, _game_after, _coordinates} = Game.shoot(context.game, context.player1, {3, 0})
+
+      assert response == :destroyed
+    end
+
+    test "should update shooter's shot_at list", context do
+      {_response, game_after, _coordinates} = Game.shoot(context.game, context.player1, {3, 0})
+      shooter = game_after.player1
+
+      assert Enum.member?(shooter.shot_at, {3, 0}) == true
+    end
+
+    test "should update opponent's got_hit_at list", context do
+      {_response, game_after, _coordinates} = Game.shoot(context.game, context.player1, {3, 0})
+      opponent = game_after.player2
+
+      assert Enum.member?(opponent.got_hit_at, {3, 0}) == true
+    end
+
+    test "should return a list with all the coordinates of the destroyed ship", context do
+      {_response, _game_after, coordinates} = Game.shoot(context.game, context.player1, {3, 0})
+
+      assert coordinates == [{0, 0}, {1, 0}, {2, 0}, {3, 0}]
+    end
+
+    test "should not change game turn to another player", context do
+      {_response, game_after, _coordinates} = Game.shoot(context.game, context.player1, {3, 0})
+
+      assert game_after.turn == :player1
+    end
+  end
+
+  describe "shoot(game, player_id, coordinates) when all opponent ships have been destroyed" do
+    setup [:place_all_ships, :destroy_all_ships]
+
+    test "should return {:game_over, game, coordinates}", context do
+      assert context.response == :game_over
+    end
+  end
+
+  describe "shoot(game, player_id, coordinates) when its not player's turn" do
+    setup [:place_all_ships]
+
+    test "should return {:not_your_turn, game, coordinates}", context do
+      {response, _game, _coordinates} =
+        Game.shoot(context.game_all_placed, context.player2, {0, 0})
+
+      assert response == :not_your_turn
+    end
+
+    test "should not change game turn to another player", context do
+      {_response, game, _coordinates} =
+        Game.shoot(context.game_all_placed, context.player2, {0, 0})
+
+      assert game.turn == :player1
+    end
+
+    test "should not update shooter's shot_at list", context do
+      {_response, game, _coordinates} =
+        Game.shoot(context.game_all_placed, context.player2, {0, 0})
+
+      shooter = game.player1
+
+      assert shooter.shot_at == []
+    end
+
+    test "should not update opponent's got_hit_at list", context do
+      {_response, game, _coordinates} =
+        Game.shoot(context.game_all_placed, context.player2, {0, 0})
+
+      opponent = game.player2
+
+      assert opponent.got_hit_at == []
+    end
+  end
+
+  defp destroy_all_ships(context) do
+    {_response, game, _coordinates} = Game.shoot(context.game_all_placed, context.player1, {0, 0})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {1, 0})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {2, 0})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {3, 0})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {0, 2})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {1, 2})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {2, 2})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {0, 4})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {1, 4})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {2, 4})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {0, 6})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {1, 6})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {0, 8})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {1, 8})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {6, 0})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {7, 0})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {6, 2})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {6, 4})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {6, 6})
+    {response, game, _coordinates} = Game.shoot(game, context.player1, {6, 8})
+
+    %{game: game, response: response}
+  end
+
+  defp shoot_three_times(context) do
+    {_response, game, _coordinates} = Game.shoot(context.game, context.player1, {0, 0})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {1, 0})
+    {_response, game, _coordinates} = Game.shoot(game, context.player1, {2, 0})
+
+    %{game: game}
+  end
+
+  defp place_one_ship(_context) do
+    coordinates = {0, 0}
+    player1 = "player1"
+    player2 = "player2"
+    game = Game.new_game(player1)
+    game = Game.join_game(game, player2)
+
+    {_response, game, _coordinates_after} =
+      Game.place_ship(game, player2, coordinates, :horizontal)
+
+    {_response, _game, _coordinates_after} =
+      Game.place_ship(game, player1, coordinates, :horizontal)
+
+    %{game: game, player1: player1, player2: player2, coordinates: coordinates}
+  end
+
+  defp place_all_ships(_context) do
+    player1 = "player1"
+    player2 = "player2"
+    game = Game.new_game(player1)
+    game = Game.join_game(game, player2)
+    orientation = :horizontal
+
+    {_response, game, _coordinates_after} = Game.place_ship(game, player2, {0, 0}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player2, {0, 2}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player2, {0, 4}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player2, {0, 6}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player2, {0, 8}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player2, {6, 0}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player2, {6, 2}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player2, {6, 4}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player2, {6, 6}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player2, {6, 8}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player1, {0, 0}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player1, {0, 2}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player1, {0, 4}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player1, {0, 6}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player1, {0, 8}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player1, {6, 0}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player1, {6, 2}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player1, {6, 4}, orientation)
+    {_response, game, _coordinates_after} = Game.place_ship(game, player1, {6, 6}, orientation)
+
+    {_response, game_all_placed, _coordinates_after} =
+      Game.place_ship(game, player1, {6, 8}, orientation)
+
+    %{game: game, game_all_placed: game_all_placed, player1: player1, player2: player2}
   end
 end
